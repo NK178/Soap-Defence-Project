ca180256b231d8f6be0860e62b3f6d2827f1675e
NK178 <ayhngern@gmail.com>
2025-09-04 16:33:40 +0800

    put da grids together

--- Assets/Script/Shop/ShopManager.cs
+++ Assets/Script/Shop/ShopManager.cs
@@ -6,11 +6,12 @@ using System.Collections.Generic;
 public class ShopManager : MonoBehaviour
 {
 
-    [SerializeField] private List<ShopItem> itemList; 
+    [SerializeField] private List<ShopItem> itemList;
+    [SerializeField] private List<GameObject> UIGrid;
 
     void Start()
     {
-        //probably should do shop UI generation here 
+        //probably should do shop UI generation here    
     }
 
     // Update is called once per frame
