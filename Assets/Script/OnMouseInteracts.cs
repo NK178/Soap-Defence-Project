abf38a67d3894a61433630c66143f477e5db1c4e
NK178 <ayhngern@gmail.com>
2025-09-06 14:19:56 +0800

    hopefully got a better mouse system now gonna test it now

--- Assets/Script/OnMouseInteracts.cs
+++ Assets/Script/OnMouseInteracts.cs
@@ -1,45 +1,102 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
-public abstract class OnMouseInteracts : MonoBehaviour
+
+[System.Serializable]
+public class MouseEventsByTag
 {
+    public string tagName;
+    public UnityEvent onMouseEnter = null;
+    public UnityEvent onMouseClick = null;
+    public UnityEvent onMouseExit = null;
+    public bool isColliding = false;
 
-    public abstract void OnMouseEnter();
-    public abstract void OnMouseDown();
-    public abstract void OnMouseExit();
 }
 
+public class OnMouseInteracts : MonoBehaviour
+{
+    //idk if this is good or bad design but I will roll with it for now 
+    [SerializeField] protected MousePositionReference mouseInstance;
+    [SerializeField] private string leftClickName;
+    [SerializeField] private List<MouseEventsByTag> responseList;
+    InputAction leftClick;
 
+    private void OnEnable()
+    {
+        leftClick = InputSystem.actions.FindAction(leftClickName);
+        if (leftClick != null)
+        {
+            leftClick.performed += HandleMouseClick;
+            leftClick.Enable();
+        }
+    }
 
-//// issue with this is that it doesnt work on prefabs so maybe need to do abstract class strat
-//public class OnMouseInteracts : MonoBehaviour
-//{
+    private void OnDisable()
+    {
+        leftClick.performed -= HandleMouseClick;
+    }
 
-//    [SerializeField] private UnityEvent onMouseEnter = null;
-//    [SerializeField] private UnityEvent onMouseClick = null;
-//    [SerializeField] private UnityEvent onMouseExit = null;
+    public void HandleMouseEnterExit()
+    {
+        for (int iter = 0; iter < responseList.Count; iter++)
+        {
 
-//    private void OnMouseEnter()
-//    {
-//        if (onMouseEnter != null)
-//        {
+            if (IsMouseColliding(responseList[iter]))
+            {
+                if (responseList[iter].onMouseEnter != null)
+                {
+                    responseList[iter].onMouseEnter.Invoke();
+                    responseList[iter].isColliding = true;
+                }
+            }
+            else
+            {
+                //dont keep repeating the exit invoke (so invoke only if is collidng is true)
+                if (responseList[iter].onMouseExit != null && responseList[iter].isColliding)
+                {
+                    responseList[iter].onMouseEnter.Invoke();
+                    responseList[iter].isColliding = false;
+                }
+            }
+        }
+    }
 
-//        }
-//    }
+    public void HandleMouseClick(InputAction.CallbackContext ctx)
+    {
 
-//    private void OnMouseDown()
-//    {
-//        if (onMouseClick != null)
-//        {
+        if (ctx.performed)
+        {
+            for (int iter = 0; iter < responseList.Count; iter++)
+            {
+                if (IsMouseColliding(responseList[iter]) && responseList[iter].onMouseClick != null)
+                {
+                    responseList[iter].onMouseClick.Invoke();
+                    break;
+                }
+            }
+        }
+    }
 
-//        }
-//    }
+    private void Update()
+    {
+        HandleMouseEnterExit();
+    }
 
-//    private void OnMouseExit()
-//    {
-//        if (onMouseExit != null)
-//        {
+    bool IsMouseColliding(MouseEventsByTag eventTag)
+    {
+        bool validCollide = false;
+        Vector2 worldMousePos = mouseInstance.GetWorldMousePos();
+        Collider2D hit = Physics2D.OverlapPoint(worldMousePos);
+        if (hit == GetComponent<Collider2D>() && hit.gameObject.CompareTag(eventTag.tagName))
+        {
+            Debug.Log("Collider2D is being hit! " + hit.gameObject.name);
+            validCollide = true;
+        }
+        return validCollide;
+    }
+}
 
-//        }
-//    }
-//}
