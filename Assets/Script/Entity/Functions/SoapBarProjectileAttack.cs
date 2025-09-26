4851c6e26fe4abd894de07d85ccbeecc33d5df82
NK178 <ayhngern@gmail.com>
2025-09-26 20:41:03 +0800

    we are back, but got lots of work to do
    I think i need to handle velocity for the projectile bruh
    probably can put it inside the set reference entity code cyka

--- Assets/Script/Entity/Functions/SoapBarProjectileAttack.cs
+++ Assets/Script/Entity/Functions/SoapBarProjectileAttack.cs
@@ -35,8 +35,8 @@ public class SoapBarProjectileAttack : EntityFunctions
             if (hit.collider != null)
                 isEnemyFound = true;
 
-
-            
+            //Testing version 
+            //I think need to handle the velocity by itself which is kinda annoying
             if (isEnemyFound)
             {
                 //calculate trajectory which will be hard bruh cyka
@@ -51,39 +51,78 @@ public class SoapBarProjectileAttack : EntityFunctions
                 RequireParentReference typeData = projectile.GetComponent<RequireParentReference>();
                 if (typeData != null)
                     typeData.SetReferenceEntity(parentObject.GetComponent<Entity>());
-                rb.linearVelocity = CalculateProjectileVelocity(parentObject, hit.collider.gameObject);
+
+                float lateralSpeed = 10f;
+                float maxHeight = 3f;
+                Vector3 fireVelocity = Vector3.zero;
+                float projGravity = 0f;
+                solve_ballistic_arc_lateral(projectile.transform.position, lateralSpeed, hit.collider.gameObject.transform.position,
+                    maxHeight, out fireVelocity, out projGravity);
+                rb.gravityScale = projGravity;
+                rb.linearVelocity = fireVelocity;
+
             }
+
+
+            //working version that isnt using lateral speed 
+            //if (isEnemyFound)
+            //{
+            //    //calculate trajectory which will be hard bruh cyka
+            //    GameObject projectile = Instantiate(soapChipPrefab, spawnPosition, parentObject.transform.rotation);
+
+
+            //    //testing 
+            //    Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            //    //rb.AddForce(Vector2.right * 20, ForceMode2D.Impulse);
+
+            //    //bad system probably needs to change but will do for now 
+            //    RequireParentReference typeData = projectile.GetComponent<RequireParentReference>();
+            //    if (typeData != null)
+            //        typeData.SetReferenceEntity(parentObject.GetComponent<Entity>());
+            //    rb.linearVelocity = CalculateProjectileVelocity(parentObject, hit.collider.gameObject);
+            //}
 
             yield return new WaitForSeconds(attackRate);
         }
     }
 
-    //public Vector3 CalculateProjectileVelocity(GameObject self, GameObject targetObj)
-    //{
-    //    float maxHeight = 3f;
-    //    float S = 30f;
-    //    Vector3 fireVelocity = Vector3.zero;
-    //    float gravity = float.NaN;
-    //    Vector3 displacement = targetObj.transform.position - self.transform.position;
-    //    float groundDist = displacement.magnitude;
 
-    //    float time = groundDist / S;
-    //    fireVelocity = displacement.normalized * S;
-    //    float a = self.transform.position.y;       // initial
-    //    float b = maxHeight;       // peak
-    //    float c = targetObj.transform.position.y;     // final
 
-    //    gravity = -4 * (a - 2 * b + c) / (time * time);
-    //    Rigidbody2D rb = targetObj.GetComponent<Rigidbody2D>();
-    //    if (rb != null)
-    //    {
-    //        float newGravityScale = gravity / Mathf.Abs(Physics2D.gravity.y);
-    //        rb.gravityScale = newGravityScale;
-    //        Debug.Log(rb.gravityScale);
-    //    }
-    //    fireVelocity.y = -(3 * a - 4 * b + c) / time;
-    //    return fireVelocity;
-    //}
+    //I dont understand this what the hell 
+    public static bool solve_ballistic_arc_lateral(Vector3 proj_pos, float lateral_speed, Vector3 target_pos, float max_height, out Vector3 fire_velocity, out float gravity)
+    {
+
+        // Handling these cases is up to your project's coding standards
+        Debug.Assert(proj_pos != target_pos && lateral_speed > 0 && max_height > proj_pos.y, "fts.solve_ballistic_arc called with invalid data");
+
+        fire_velocity = Vector3.zero;
+        gravity = float.NaN;
+
+        Vector3 diff = target_pos - proj_pos;
+        Vector3 diffXZ = new Vector3(diff.x, 0f, diff.z);
+        float lateralDist = diffXZ.magnitude;
+
+        if (lateralDist == 0)
+            return false;
+
+        float time = lateralDist / lateral_speed;
+
+        fire_velocity = diffXZ.normalized * lateral_speed;
+
+        // System of equations. Hit max_height at t=.5*time. Hit target at t=time.
+        //
+        // peak = y0 + vertical_speed*halfTime + .5*gravity*halfTime^2
+        // end = y0 + vertical_speed*time + .5*gravity*time^s
+        // Wolfram Alpha: solve b = a + .5*v*t + .5*g*(.5*t)^2, c = a + vt + .5*g*t^2 for g, v
+        float a = proj_pos.y;       // initial
+        float b = max_height;       // peak
+        float c = target_pos.y;     // final
+
+        gravity = -4 * (a - 2 * b + c) / (time * time);
+        fire_velocity.y = -(3 * a - 4 * b + c) / time;
+
+        return true;
+    }
 
     //this works, lets see if it can be better 
     public Vector3 CalculateProjectileVelocity(GameObject self, GameObject targetObj)
@@ -106,7 +145,7 @@ public class SoapBarProjectileAttack : EntityFunctions
         float highAng = Mathf.Atan2(speed2 + root, gx);
 
         finalVector = displacement.normalized * Mathf.Cos(highAng) * S + Vector3.up * Mathf.Sin(highAng) * S;
-        Debug.Log("FINAL VECTOR " + finalVector);
+        //Debug.Log("FINAL VECTOR " + finalVector);
         return finalVector;
     }
 }
