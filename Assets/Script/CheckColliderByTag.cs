f49da11e2c7d7c4a2f1a9413ac7efce51ed116b5
NK178 <ayhngern@gmail.com>
2025-10-03 21:59:33 +0800

    bruh wtver stupid bug

--- Assets/Script/CheckColliderByTag.cs
+++ Assets/Script/CheckColliderByTag.cs
@@ -20,7 +20,6 @@ public class CheckColliderByTag : MonoBehaviour
     {
         foreach (EventCollideByTag target in list)
         {
-            Debug.Log(this.name + " COLLIDING WITH " + collision.gameObject.name + " TAG: " + collision.gameObject.tag);
             if (target.tag == collision.gameObject.tag)
             {
                 //Debug.Log("COLLIDING WITH " + collision.gameObject.name);
